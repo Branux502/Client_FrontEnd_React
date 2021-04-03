80d831c8c4a7337d11739d109c67cbde855ddf8d
Brayan Miranda <brayanmiranda333@gmail.com>
2021-04-03 16:24:07 -0600

    v2 show up my own api

--- src/App.js
+++ src/App.js
@@ -1,35 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
-import { useEffect, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 import { useLayoutEffect } from 'react/cjs/react.development';
+import axios from "axios";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Modal,ModalBody,ModalFooter,ModalHeader} from 'reactstrap';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faEdit, faTrashAlt} from '@fortawesome/free-solid-svg-icons';
 
-function App() {
 
-  const[Student, setStudent] = useState([])
-  useEffect(() => {
-    ObtenerDatos()
-  }, [] )
+const url="http://localhost:8090/api/v1/student"
 
-  const ObtenerDatos = async () => {
-    const data = await fetch('http://localhost:8090/api/v1/student')
-    const users = await data.json()
-    setStudent(users)
+class App extends Component {
+
+  state={
+    data:[]
+  }
+
+  peticionGet=()=>{
+    axios.get(url).then(response=>{
+      console.log(response.data)
+    })
+  }
+
+  componentDidMount(){
+    this.peticionGet();
   }
 
 
+  render(){
   return (
 
-        <div>
-          <ul>
-            {
-              Student.map( item => (
-                <li key={item.id}>{item.name} - {item.email} - {item.dob} - {item.age}</li>
-                ))
-            }           
-          </ul>
+        <div classname="App">
+          <br></br>
+          <button className="btn btn-success">Agregar Estudiante</button>
+          <br></br>
+            <table>
+              <thead>
+                <tr>
+                  <th>Id</th>
+                  <th>Nombre</th>
+                  <th>Correo</th>
+                  <th>DOB</th>
+                  <th>Edad</th>
+                </tr>
+              </thead>
+            </table>
+
+
+          
         </div>
         
   );
 }
-
+}
 export default App;
