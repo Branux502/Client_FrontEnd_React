cd4176fa628784fd242dd94233c7124b3e1a4d17
Brayan Miranda <brayanmiranda333@gmail.com>
2021-04-03 19:38:20 -0600

    v4 get put working

--- src/App.js
+++ src/App.js
@@ -14,14 +14,46 @@ const url="http://localhost:8090/api/v1/student"
 class App extends Component {
 
   state={
-    data:[]
+    data:[],
+    modalInsertar: false,
+    form:{
+      name: '',
+      email: '',
+      dob: ''
+    }
   }
 
   peticionGet=()=>{
     axios.get(url).then(response=>{
       this.setState({data:response.data});
-
+    }).catch(error=>{
+      console.log(error.message);
     })
+  }
+
+  peticionPost=async ()=>{
+   await axios.post(url,this.state.form).then(response=>{
+      this.modalInsertar();
+      this.peticionGet();
+    }).catch(error=>{
+      console.log(error.message);
+    })
+  }
+
+  modalInsertar=()=>{
+    this.setState({modalInsertar: !this.state.modalInsertar});
+  }
+
+  handleChange=async e=>{
+    e.persist();
+    await this.setState({
+      form:{
+        ...this.state.form,
+        [e.target.name]: e.target.value
+      }
+    }
+    );
+    console.log(this.state.form);
   }
 
   componentDidMount(){
@@ -30,11 +62,12 @@ class App extends Component {
 
 
   render(){
+    const {form}=this.state;
   return (
 
         <div classname="App">
-          <br></br>
-          <button className="btn btn-success">Agregar Estudiante</button>
+          <br /><br /><br />
+          <button className="btn btn-success" onClick={()=>this.modalInsertar()}>Agregar Estudiante</button>
           <br></br>
             <table className="table">
               <thead>
@@ -68,9 +101,37 @@ class App extends Component {
                 }
               </tbody>
             </table>
+            
+            <Modal isOpen={this.state.modalInsertar}>
+                <ModalHeader style={{display: 'block'}}>
+                  <span style={{float: 'right'}} onClick={()=>this.modalInsertar()}>x</span>
+                </ModalHeader>
+                <ModalBody>
+                  <div className="form-group">
+                    <label htmlFor="nombre">Nombre</label>
+                    <input className="form-control" type="text" name="name" onChange={this.handleChange} value={form.name}/>
+                    <br />
+                    <label htmlFor="nombre">Email</label>
+                    <input className="form-control" type="text" name="email" onChange={this.handleChange} value={form.email}/>
+                    <br />
+                    <label htmlFor="capital_bursatil">DOB</label>
+                    <input className="form-control" type="text"  name="dob" onChange={this.handleChange} value={form.dob}/>
+                    <br />
+                  </div>
+                </ModalBody>
+
+                <ModalFooter>
+                  
+                    <button className="btn btn-success" onClick={()=>this.peticionPost()}>
+                    Insertar
+                  </button> 
+                  
+                    <button className="btn btn-danger" onClick={()=>this.modalInsertar()}>Cancelar</button>
+                </ModalFooter>
+          </Modal>
 
 
-          
+
         </div>
         
   );
